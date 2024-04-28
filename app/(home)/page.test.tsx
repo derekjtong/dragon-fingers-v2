ea24cd2f7a2dd56e31b89b1b645604fa044d82e7
Derek <109226053+derekjtong@users.noreply.github.com>
2024-04-28 18:42:38 -0400

    Add home page tests

--- app/(home)/page.test.tsx
+++ app/(home)/page.test.tsx
@@ -4,5 +4,15 @@ import HomePage from "./page";
 
 test("Home Page Loads", () => {
   render(<HomePage />);
-  expect(screen.getByRole("heading", { level: 1, name: "Home Page" })).toBeDefined();
+  const enterRaceLink = screen.getByRole("link", { name: "Enter Race" });
+  expect(enterRaceLink).toBeDefined();
+  expect(enterRaceLink.getAttribute("href")).toBe("/game/temp-game");
+
+  const joinCustomLink = screen.getByRole("link", { name: "Join Custom" });
+  expect(joinCustomLink).toBeDefined();
+  expect(joinCustomLink.getAttribute("href")).toBe("/game/join");
+
+  const createCustomLink = screen.getByRole("link", { name: "Create Custom" });
+  expect(createCustomLink).toBeDefined();
+  expect(createCustomLink.getAttribute("href")).toBe("/game/create");
 });
