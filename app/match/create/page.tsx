b750d63650712b8a894ec1e8f996fc81a125b9b5
Derek <109226053+derekjtong@users.noreply.github.com>
2024-04-29 03:22:55 -0400

    Add error handling for unauthenticated users creating matches

--- app/match/create/page.tsx
+++ app/match/create/page.tsx
@@ -1,17 +1,32 @@
 "use client";
 import axios from "axios";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 function CreatePage() {
+  const [error, setError] = useState<string>("");
+  const session = useSession();
   const router = useRouter();
+
   const handleClick = async () => {
+    if (session.status === "loading") {
+      console.log("Session is loading");
+      return;
+    }
+    if (session.status === "unauthenticated") {
+      setError("Must be logged in");
+      return;
+    }
+
+    console.log(session);
     try {
       const response = await axios.get("/api/match/new");
       const match = response.data;
       router.push(`/match/${match.id}`);
     } catch (error) {
       console.error("Error creating match:", error);
-      alert("Failed to create match!");
+      setError("Failed to create match");
     }
   };
   return (
@@ -23,6 +38,7 @@ function CreatePage() {
       <button className="mt-10 p-3 text-2xl hover:bg-gray-300" onClick={handleClick}>
         Create Match
       </button>
+      <div className="text-red-500">{error}</div>
     </div>
   );
 }
