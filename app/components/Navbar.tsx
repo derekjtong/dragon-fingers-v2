aa218b9a10ff0d7b1a552854ec03b0faa7ed97b1
Derek <109226053+derekjtong@users.noreply.github.com>
2024-04-29 03:22:35 -0400

    Update app name

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <div className="fixed flex w-screen justify-between px-72 pt-10 text-2xl">
       <Link href="/" className="p-3 hover:bg-slate-300">
-        Home
+        Dragon Fingers
       </Link>
       <AuthButton />
       {/* <Link href="/login" className="p-3 hover:bg-slate-300">
