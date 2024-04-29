dce797930be5f95c98872f024a07794ff9cdb5e6
Derek <109226053+derekjtong@users.noreply.github.com>
2024-04-29 17:25:33 -0400

    Add dropdown menu to AuthButton

--- app/components/AuthButton.tsx
+++ app/components/AuthButton.tsx
@@ -1,5 +1,14 @@
 "use client";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { AvatarFallback } from "@radix-ui/react-avatar";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
@@ -12,15 +21,34 @@ function AuthButton() {
   if (session.status === "authenticated") {
     return (
       <div className="flex">
-        <Link className="cursor-pointer p-3 hover:bg-gray-300" href="/profile">
-          <Avatar>
-            <AvatarImage src={session.data.user?.image || undefined} />
-            <AvatarFallback>{session.data.user?.name}</AvatarFallback>
-          </Avatar>
-        </Link>
-        <div className="cursor-pointer p-3 hover:bg-gray-300" onClick={() => signOut()}>
-          Sign out
-        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Avatar className="h-14 w-14 cursor-pointer">
+              <AvatarImage src={session.data.user?.image || undefined} />
+              <AvatarFallback>{session.data.user?.name}</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Hello {session.data.user?.name}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="cursor-pointer justify-center">
+                <Link href="/profile">Profile</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer justify-center">Settings</DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer justify-center">
+              <a target="_blank" href="https://github.com/derekjtong/dragon-fingers-v2">
+                GitHub
+              </a>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer justify-center" onSelect={() => signOut()}>
+              Log out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     );
   }
