56461d0175e2a9b2280ecf4784ff243c1f9c3df8
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-11 17:48:06 -0400

    Add user data endpoint

---
+++ app/api/user/route.ts
@@ -0,0 +1,30 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    const currentUser = await getCurrentUser();
+    if (!currentUser?.id || !currentUser?.email) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const userStats = await prisma?.stats.findUnique({
+      where: {
+        userId: currentUser.id,
+      },
+    });
+
+    const userData = {
+      id: currentUser.id,
+      name: currentUser.name,
+      email: currentUser.email,
+      createdAt: currentUser.createdAt,
+      isAdmin: currentUser.isAdmin,
+      averageSpeed: userStats?.averageSpeed,
+      bestSpeed: userStats?.bestSpeed,
+      matchesPlayed: userStats?.matchesPlayed,
+      matchesWon: userStats?.matchesWon,
+    };
+    return new NextResponse(JSON.stringify(userData), { status: 200 });
+  } catch (error: any) {}
+}
