99805e73c3f1c7bb6b96e3bd567bf3a6ac99894b
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-11 17:48:26 -0400

    Add user data message type

--- app/types/index.ts
+++ app/types/index.ts
@@ -7,14 +7,29 @@ type ExtendedParticipant = {
   speed: number | null;
   accuracy: number | null;
   user: {
+    id: string;
     name: string;
     image: string;
   };
 };
 
 // Progress message used in pusher channels
-type ProgressUpdateType = {
+type MatchUpdateMessage = {
   name: string;
   userId: string;
   wordCount: number;
+};
+
+// User Data
+// See api/user get
+type UserData = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+  isAdmin: boolean;
+  averageSpeed: number;
+  bestSpeed: number;
+  matchesPlayed: number;
+  matchesWon: number;
 };
