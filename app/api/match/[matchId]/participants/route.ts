4f0b5614220a282faeab478f8ec8abfd2e5d9b90
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-12 01:52:13 -0400

    Add join match route

--- app/api/match/[matchId]/participants/route.ts
+++ app/api/match/[matchId]/participants/route.ts
@@ -1,12 +1,18 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import prisma from "@/app/libs/prismadb";
+import { pusherServer } from "@/app/libs/pusher";
 import { NextResponse } from "next/server";
+
 interface IParams {
-  matchId?: string;
+  matchId: string;
 }
+
+// Get all match participants
 export async function GET(request: Request, { params }: { params: IParams }) {
   try {
     const { matchId } = params;
 
+    // Retrieve match participants from database
     const participants = await prisma.participant.findMany({
       where: {
         matchId: matchId,
@@ -21,8 +27,79 @@ export async function GET(request: Request, { params }: { params: IParams }) {
         },
       },
     });
+
     return NextResponse.json(participants);
   } catch (error) {
     console.error("Error fetching participants:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+// Join match
+export async function POST(request: Request, { params }: { params: IParams }) {
+  try {
+    const currentUser = await getCurrentUser();
+    const { matchId } = params;
+
+    // Validate user
+    if (!currentUser?.id || !currentUser?.email) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    // Validate match
+    if (!matchId || !/^[0-9a-fA-F]{24}$/.test(matchId)) {
+      return new NextResponse("Match not found", { status: 404 });
+    }
+
+    // Find match
+    const match = await prisma.match.findUnique({
+      where: {
+        id: matchId,
+      },
+    });
+
+    // Validate match
+    if (!match) {
+      return new NextResponse("Match not found", { status: 404 });
+    }
+
+    // Do not allow users to join not open matches
+    if (!match.open) {
+      return new NextResponse("Match not open for new participants", { status: 403 });
+    }
+
+    // Send update
+    await pusherServer.trigger(matchId, "progress-update", {
+      name: currentUser.name,
+      userId: currentUser.id,
+      charCount: 0,
+      end: false,
+    });
+
+    // Search participant related to user and match
+    const existingParticipant = await prisma.participant.findFirst({
+      where: {
+        matchId: matchId,
+        userId: currentUser?.id,
+      },
+    });
+
+    // If already exists return it
+    if (existingParticipant) {
+      return NextResponse.json(existingParticipant);
+    }
+
+    // Create it if it does not exist
+    const newParticipant = await prisma.participant.create({
+      data: {
+        matchId: matchId,
+        userId: currentUser.id,
+      },
+    });
+
+    return new NextResponse("Participant added", { status: 201 });
+  } catch (error: any) {
+    console.error("Error posting participant:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
