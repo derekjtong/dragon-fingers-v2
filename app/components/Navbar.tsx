4eace9d4553a21ff2358950146e2774dd4dd9b57
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-12 13:28:22 -0400

    Support small screens

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import AuthButton from "./AuthButton";
 
 function NavBar() {
   return (
-    <div className="fixed flex w-screen justify-between px-72 pt-10 text-2xl">
+    <div className="fixed flex w-screen justify-between px-9 pt-10 text-2xl lg:px-72">
       <Link href="/" className="p-3 hover:bg-slate-300">
         Dragon Fingers
       </Link>
