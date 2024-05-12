74ab0632d109821cedec8d9acca4caf2d2d1da77
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-12 02:29:18 -0400

    Add end match api route

--- app/api/match/[matchId]/route.ts
+++ app/api/match/[matchId]/route.ts
@@ -16,6 +16,61 @@ export async function GET(request: Request, { params }: { params: IParams }) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
+    if (!matchId || !/^[0-9a-fA-F]{24}$/.test(matchId)) {
+      return new NextResponse("Match not found", { status: 404 });
+    }
+
+    const match = await prisma.match.findUnique({
+      where: {
+        id: matchId,
+      },
+    });
+
+    if (!match) {
+      return new NextResponse("No match found with the given code", { status: 404 });
+    }
+
+    return NextResponse.json(match);
+  } catch (err) {
+    console.error("Error fetching match:", err);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+// Update word count
+export async function POST(request: Request, { params }: { params: IParams }) {
+  try {
+    const { matchId } = params;
+    const currentUser = await getCurrentUser();
+    const body = await request.json();
+    const { charCount } = body;
+
+    if (!currentUser?.id || !currentUser?.email) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    await pusherServer.trigger(matchId, "progress-update", {
+      name: currentUser.name,
+      userId: currentUser.id,
+      charCount,
+      status: "",
+    });
+
+    return new NextResponse("Success", { status: 200 });
+  } catch (error: any) {
+    console.log("Error updating match:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
+
+// End a match
+export async function PATCH(request: Request, { params }: { params: IParams }) {
+  try {
+    // Record current time
+    const endTime = new Date();
+
+    const { matchId } = params;
+
     // Validate matchId format
     if (!matchId || !/^[0-9a-fA-F]{24}$/.test(matchId)) {
       return new NextResponse("Match not found", { status: 404 });
@@ -28,54 +83,38 @@ export async function GET(request: Request, { params }: { params: IParams }) {
       },
     });
 
-    // Check if the match was found
-    if (match) {
-      // Send a 0 progress to alert join
-      await pusherServer.trigger(matchId, "progress-update", {
-        name: currentUser.name,
-        userId: currentUser.id,
-        wordCount: 0,
-      });
-
-      // Return the match data in JSON format
-      return new NextResponse(JSON.stringify(match), {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        status: 200,
-      });
-    } else {
-      // If no match is found, return a 404 Not Found response
-      return new NextResponse("No match found with the given code", { status: 404 });
-    }
-  } catch (err) {
-    // Log the error and return a 500 Internal Server Error response
-    console.error("Error fetching match:", err);
-    return new NextResponse("Internal Server Error", { status: 500 });
-  }
-}
-
-// Update word count
-export async function POST(request: Request, { params }: { params: IParams }) {
-  try {
-    const { matchId } = params;
-    const currentUser = await getCurrentUser();
-    const body = await request.json();
-    const { wordCount } = body;
-
-    if (!currentUser?.id || !currentUser?.email) {
-      return new NextResponse("Unauthorized", { status: 401 });
+    // Check if match exists
+    if (!match) {
+      return new NextResponse("Match not found", { status: 404 });
     }
 
+    // Check if match is already closed
+    if (!match.open) {
+      return new NextResponse("Match is already closed", { status: 409 });
+    }
+
+    // Send end game message
     await pusherServer.trigger(matchId, "progress-update", {
-      name: currentUser.name,
-      userId: currentUser.id,
-      wordCount,
+      name: "admin",
+      userId: "whatever",
+      charCount: 0,
+      status: "closed",
     });
 
-    return new NextResponse("Success", { status: 200 });
+    // Update record
+    const updatedMatch = await prisma.match.update({
+      where: {
+        id: matchId,
+      },
+      data: {
+        endTime,
+        open: false,
+      },
+    });
+
+    return new NextResponse("Ended match", { status: 200 });
   } catch (error: any) {
-    console.log("Error updating match:", error);
+    console.log("Error ending match:", error);
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
