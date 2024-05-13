f9c8f031ad4d74220895ecbacc6b98f9810d7373
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-13 15:54:14 -0400

    Responsive home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <Link href="/match/joinexisting" className="m-20">
+    <div className="flex h-full flex-col items-center justify-center ">
+      <Link href="/match/joinexisting" className="m-24 lg:mt-44">
         <Button size={"lg"} className="h-40 w-96 text-4xl">
           Enter Race
         </Button>
