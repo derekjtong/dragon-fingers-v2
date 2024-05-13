1586d7c703268ece39ba0ef9f563972dbf98914d
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-13 12:57:41 -0400

    Adjust border

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import AuthButton from "./AuthButton";
 
 function NavBar() {
   return (
-    <div className="fixed flex w-screen justify-between px-9 pt-10 text-2xl lg:px-72">
+    <div className="fixed z-50 flex w-screen justify-between border bg-white px-9 py-10 text-2xl lg:px-72">
       <Link href="/" className="p-3 font-mono text-4xl hover:bg-slate-300">
         Dragon Fingers
       </Link>
