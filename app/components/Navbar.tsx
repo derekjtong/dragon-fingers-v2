f184a9c27d0b1d7a9fe793ffc926232448d33ed3
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-13 12:54:27 -0400

    Adjust logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import AuthButton from "./AuthButton";
 function NavBar() {
   return (
     <div className="fixed flex w-screen justify-between px-9 pt-10 text-2xl lg:px-72">
-      <Link href="/" className="p-3 hover:bg-slate-300">
+      <Link href="/" className="p-3 font-mono text-4xl hover:bg-slate-300">
         Dragon Fingers
       </Link>
       <AuthButton />
