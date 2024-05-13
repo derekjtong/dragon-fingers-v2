17a7fe24e86cb90359cfcc1d2021a7340734e350
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-13 12:44:26 -0400

    Adjust margin

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,22 +4,22 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <Link href="/match/joinexisting" className="p-20">
+      <Link href="/match/joinexisting" className="m-20">
         <Button size={"lg"} className="h-40 w-96 text-4xl">
           Enter Race
         </Button>
       </Link>
-      <Link href="/match/solo" className="p-2">
+      <Link href="/match/solo" className="m-2">
         <Button variant={"outline"} className="h-12 w-48">
           Solo
         </Button>
       </Link>
-      <Link href="/match/join" className="p-2">
+      <Link href="/match/join" className="m-2">
         <Button variant={"outline"} className="h-12 w-48">
           Join Custom
         </Button>
       </Link>
-      <Link href="/match/create" className="p-2 ">
+      <Link href="/match/create" className="m-2 ">
         <Button variant={"outline"} className="h-12 w-48">
           Create Custom
         </Button>
