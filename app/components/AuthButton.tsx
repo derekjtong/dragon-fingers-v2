2c08139cebe658470e18f6bb623dc1b94fced177
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-13 12:07:48 -0400

    Remove auth button drop down menu

--- app/components/AuthButton.tsx
+++ app/components/AuthButton.tsx
@@ -1,16 +1,7 @@
 "use client";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { AvatarFallback } from "@radix-ui/react-avatar";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useState } from "react";
 
@@ -23,36 +14,12 @@ function AuthButton() {
   if (session.status === "authenticated") {
     return (
       <div className="flex">
-        <DropdownMenu open={openDropdown} onOpenChange={() => setOpenDropdown(false)}>
-          <DropdownMenuTrigger onMouseEnter={() => setOpenDropdown(true)}>
-            <Avatar className="h-14 w-14 cursor-pointer focus:outline-none">
-              <AvatarImage src={session.data.user?.image || undefined} />
-              <AvatarFallback>{session.data.user?.name}</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent onMouseLeave={() => setOpenDropdown(false)}>
-            <DropdownMenuLabel>Hello {session.data.user?.name}</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem className="cursor-pointer justify-center">
-                <Link href="/profile">Profile</Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="cursor-pointer justify-center">
-                <Link href="/settings">Settings</Link>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="cursor-pointer justify-center">
-              <a target="_blank" href="https://github.com/derekjtong/dragon-fingers-v2">
-                GitHub
-              </a>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="cursor-pointer justify-center" onSelect={() => signOut()}>
-              Log out
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <Link href="/profile">
+          <Avatar className="h-14 w-14 cursor-pointer focus:outline-none">
+            <AvatarImage src={session.data.user?.image || undefined} />
+            <AvatarFallback>{session.data.user?.name}</AvatarFallback>
+          </Avatar>
+        </Link>
       </div>
     );
   }
