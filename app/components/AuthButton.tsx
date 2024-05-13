34764b594db4dd135575cd7b5047c3f9c2d4d939
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-13 12:26:37 -0400

    Add logout icon

--- app/components/AuthButton.tsx
+++ app/components/AuthButton.tsx
@@ -1,28 +1,30 @@
 "use client";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { AvatarFallback } from "@radix-ui/react-avatar";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import { useState } from "react";
+import { FaSignOutAlt } from "react-icons/fa";
 
 function AuthButton() {
-  const [openDropdown, setOpenDropdown] = useState(false);
   const session = useSession();
-  if (session.status === "loading") {
-    return <div></div>;
-  }
+
   if (session.status === "authenticated") {
     return (
-      <div className="flex">
+      <div className="flex items-center justify-center">
         <Link href="/profile">
           <Avatar className="h-14 w-14 cursor-pointer focus:outline-none">
             <AvatarImage src={session.data.user?.image || undefined} />
             <AvatarFallback>{session.data.user?.name}</AvatarFallback>
           </Avatar>
         </Link>
+        <Button className="ml-2" variant="ghost" onClick={() => signOut()}>
+          <FaSignOutAlt size={25} />
+        </Button>
       </div>
     );
   }
+
   return (
     <div className="cursor-pointer p-3 hover:bg-gray-300" onClick={() => signIn()}>
       Login
