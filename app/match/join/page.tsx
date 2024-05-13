596a929a305202f3cfc3fc9e01e6c721950b644e
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-13 16:02:40 -0400

    Responsive join page

--- app/match/join/page.tsx
+++ app/match/join/page.tsx
@@ -62,8 +62,8 @@ function JoinPage() {
   };
 
   return (
-    <div className="flex w-screen">
-      <div className="flex h-screen w-1/2 flex-col items-center justify-center text-2xl">
+    <div className="flex w-screen flex-col items-center pt-20 md:flex-row md:items-start md:pt-0">
+      <div className="flex w-1/2 flex-col items-center justify-center text-2xl md:h-screen">
         <div className="mb-7 text-4xl">Join a game</div>
         <form onSubmit={handleSubmit} className="flex flex-col">
           <input
@@ -81,16 +81,20 @@ function JoinPage() {
         <div className="flex flex-col items-center">
           <div className="mb-7 text-4xl">Active Match List</div>
           <div>
-            {matches.map((match) => (
-              <div className="flex items-center justify-center" key={match.id}>
-                <div>{match.id}</div>
-                <Link href={`/match/${match.id}`} className="ml-2">
-                  <Button>
-                    Join <FaArrowRight />
-                  </Button>
-                </Link>
-              </div>
-            ))}
+            {matches.length !== 0 ? (
+              matches.map((match) => (
+                <div className="flex items-center justify-center" key={match.id}>
+                  <div>{match.id}</div>
+                  <Link href={`/match/${match.id}`} className="ml-2">
+                    <Button>
+                      Join <FaArrowRight />
+                    </Button>
+                  </Link>
+                </div>
+              ))
+            ) : (
+              <div>No Active Matches</div>
+            )}
           </div>
         </div>
       </div>
