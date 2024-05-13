6d4da8e9806a57d360ce26d8f8e3e7443ecc6112
Derek <109226053+derekjtong@users.noreply.github.com>
2024-05-13 18:09:34 -0400

    Send only short texts

--- app/actions/getAllTexts.ts
+++ app/actions/getAllTexts.ts
@@ -2,7 +2,8 @@ import prisma from "@/app/libs/prismadb";
 
 const getTexts = async () => {
   try {
-    const texts = await prisma.text.findMany();
+    const allTexts = await prisma.text.findMany();
+    const texts = allTexts.filter((text) => text.body.length <= 70);
     return texts;
   } catch (error: any) {
     return [];
